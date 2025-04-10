b830c100d7f98de469e978c4cf3f8a46e5974eda
ShahwaizM <shahwaizmughal02@gmail.com>
2025-04-10 21:24:10 +0500

    Initial commit

--- Server/index.js
+++ Server/index.js
@@ -1,38 +1,9 @@
-// import express from "express";
-// import mongoose from "mongoose";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import orderRoutes from "./Routes/OrderRoutes.js";
-
-// dotenv.config();
-// const app = express();
-
-// app.use(express.json());
-// app.use(cors());
-
-// const PORT = process.env.PORT || 5000;
-// const MONGO_URI = process.env.MONGO_URI;
-
-// mongoose
-//   .connect(MONGO_URI)
-//   .then(() => console.log("MongoDB Connected"))
-//   .catch((err) => console.log(err));
-
-// app.use("/api/orders", orderRoutes);
-
-// app.get("/", (req, res) => {
-//   res.send("Server is running...");
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on http://localhost:${PORT}`);
-// });
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import serverless from "serverless-http"; // Import the serverless-http module
 import orderRoutes from "./Routes/OrderRoutes.js";
-import serverless from "serverless-http"; // Serverless function handler
 
 dotenv.config();
 const app = express();
@@ -54,5 +25,5 @@ app.get("/", (req, res) => {
   res.send("Server is running...");
 });
 
-// Export serverless function
-module.exports.handler = serverless(app);
+// Wrap your Express app using serverless-http
+export default serverless(app);
