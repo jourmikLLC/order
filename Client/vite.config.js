661333b1124b3d69f67d8297d58572d98bca0477
ShahwaizM <shahwaizmughal02@gmail.com>
2025-04-10 23:41:13 +0500

    Initial commit

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,20 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+// https://vite.dev/config/
+;export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  },
   server: {
     proxy: {
       '/api': {
         target: 'https://order-two-gamma.vercel.app/',
-        changeOrigin: true,
-        secure: true, // Set to true for HTTPS
-        logLevel: 'debug',
+        changeOrigin: true, // For handling CORS issues
+        secure: false, // Ensure it's true if you're using HTTPS
+        logLevel: 'debug', // Log proxy actions for debugging
       },
     },
-    historyApiFallback: true // Ensures React Router works on refresh
-  }
+  },
 });
+
