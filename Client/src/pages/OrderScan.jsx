de84baaef2c392e94c744888ce88c48657323934
MUHAMMAD SHAHWAIZ MUGHAL <86921257+ShahwaizM@users.noreply.github.com>
2025-04-15 00:26:20 +0500

    Update OrderScan.jsx

--- Client/src/pages/OrderScan.jsx
+++ Client/src/pages/OrderScan.jsx
@@ -6,7 +6,7 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 message.config({
   top: 100,
-  duration: 4,
+  duration: 3,
 });
 
 function OrdersScan() {
@@ -18,7 +18,6 @@ function OrdersScan() {
   const [scannedParts, setScannedParts] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  // Fetch Order by Tracking ID
   const fetchOrder = async () => {
     setLoading(true);
     try {
@@ -48,7 +47,6 @@ function OrdersScan() {
     setLoading(false);
   };
 
-  // Verify Part Number
   const verifyPartNumber = async () => {
     if (!scannedPart) {
       message.error("⚠️ Please enter a Part Number.");
@@ -56,7 +54,7 @@ function OrdersScan() {
     }
 
     if (scannedParts.includes(scannedPart)) {
-      message.warning("⚠️ This Part Number has already been scanned.");
+      message.warning("⚠️ This part has already been scanned.");
       setScannedPart("");
       return;
     }
@@ -74,7 +72,7 @@ function OrdersScan() {
 
       const data = await response.json();
 
-      if (data.message === "Part Number matched.") {
+      if (data.message === "Part number matched.") {
         setScannedParts([...scannedParts, scannedPart]);
         setScannedPart("");
 
@@ -85,16 +83,117 @@ function OrdersScan() {
           setCurrentPartIndex(currentPartIndex + 1);
           message.success(` Part ${currentPartIndex + 1} matched.`);
         } else {
-          message.success(
-            "🎉 All Part Numbers matched. Order ready for dispatch!"
-          );
-          setOrder(null);
-          setTrackingId("");
-          setTrackingIdValid(false);
-          setScannedParts([]);
+          message.success("🎉 All parts matched. Order ready for dispatch!");
+          resetState();
         }
-      } else if (data.message.includes("Wrong Part Number")) {
+      } else {
         message.error(" Wrong Part Number. Please scan again.");
+      }
+    } catch (error) {
+      message.error("⚠️ Error verifying Part Number. Try again.");
+    }
+    setLoading(false);
+  };
+
+  const resetState = () => {
+    setOrder(null);
+    setTrackingId("");
+    setTrackingIdValid(false);
+    setScannedParts([]);
+  };
+
+  const handleKeyPress = (event, type) => {
+    if (event.key === "Enter") {
+      if (type === "tracking") fetchOrder();
+      if (type === "part") verifyPartNumber();
+    }
+  };
+
+  const handlePaste = async (e, type) => {
+    e.preventDefault();
+    const pastedText = (e.clipboardData || window.clipboardData)
+      .getData("text")
+      .trim();
+
+    if (type === "tracking") {
+      setTrackingId(pastedText);
+      fetchOrderWithValue(pastedText); // pass directly
+    }
+
+    if (type === "part") {
+      setScannedPart(pastedText);
+      verifyPartNumberWithValue(pastedText); // pass directly
+    }
+  };
+  const fetchOrderWithValue = async (pastedTrackingId) => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `${API_URL}/orders/scan/validateTrackingId`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ trackingId: pastedTrackingId }),
+        }
+      );
+
+      const data = await response.json();
+      if (data.order) {
+        setOrder(data.order);
+        setTrackingIdValid(true);
+        setCurrentPartIndex(0);
+        setScannedPart("");
+        setScannedParts([]);
+        message.success(`Tracking ID verified: ${pastedTrackingId}`);
+      } else {
+        message.error("Invalid Tracking ID. Please try again.");
+      }
+    } catch (error) {
+      message.error("⚠️ Error fetching order. Try again.");
+    }
+    setLoading(false);
+  };
+
+  const verifyPartNumberWithValue = async (pastedPartNumber) => {
+    if (!pastedPartNumber) {
+      message.error("⚠️ Please enter a Part Number.");
+      return;
+    }
+
+    if (scannedParts.includes(pastedPartNumber)) {
+      message.warning("⚠️ This part has already been scanned.");
+      return;
+    }
+
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `${API_URL}/orders/scan/validatePartNumbers`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ trackingId, partNumber: pastedPartNumber }),
+        }
+      );
+
+      const data = await response.json();
+
+      if (data.message === "Part number matched.") {
+        setScannedParts([...scannedParts, pastedPartNumber]);
+        setScannedPart("");
+
+        if (
+          currentPartIndex + 1 <
+          order.entries.flatMap((entry) => entry.partNumbers).length
+        ) {
+          setCurrentPartIndex(currentPartIndex + 1);
+          message.success(`Part ${currentPartIndex + 1} matched.`);
+        } else {
+          message.success("🎉 All parts matched. Order ready for dispatch!");
+          resetState();
+        }
+      } else {
+        message.error("Wrong Part Number. Please scan again.");
       }
     } catch (error) {
       message.error("⚠️ Error verifying Part Number. Try again.");
@@ -108,18 +207,17 @@ function OrdersScan() {
       style={{
         display: "flex",
         justifyContent: "center",
+        fontSize: "70px",
         alignItems: "center",
         height: "100vh",
         backgroundColor: "#f8d9fa",
       }}
     >
       <Card
-        title="📦 Order Scanning"
         style={{
-          width: "600px",
           padding: "20px",
           textAlign: "center",
-          fontSize: "22px",
+          fontSize: "70px",
           fontWeight: "bold",
           boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
           borderRadius: "10px",
@@ -127,31 +225,31 @@ function OrdersScan() {
       >
         {!trackingIdValid ? (
           <>
-            <p style={{ fontSize: "18px" }}>
+            <p style={{ fontSize: "70px" }}>
               Enter Tracking ID to fetch order details:
             </p>
             <Input
               value={trackingId}
               onChange={(e) => setTrackingId(e.target.value)}
+              onKeyPress={(e) => handleKeyPress(e, "tracking")}
+              onPaste={(e) => handlePaste(e, "tracking")} // pass the event
               placeholder="🔍 Scan Tracking ID"
-              style={{ fontSize: "16px", padding: "10px" }}
+              style={{ fontSize: "70px", padding: "10px" }}
+              autoFocus
             />
+
             <Button
               type="primary"
               style={{
                 marginTop: "10px",
                 width: "100%",
-                fontSize: "18px",
-                padding: "15px 20px",
-                paddingBottom: "40px",
+                fontSize: "40px",
+                padding: "20px 20px 80px",
                 fontWeight: "bold",
                 borderRadius: "8px",
                 backgroundColor: "#1890ff",
                 border: "none",
-                transition: "all 0.3s ease",
               }}
-              onMouseEnter={(e) => (e.target.style.backgroundColor = "#1890ff")}
-              onMouseLeave={(e) => (e.target.style.backgroundColor = "#1890ff")}
               onClick={fetchOrder}
               disabled={loading}
             >
@@ -159,44 +257,45 @@ function OrdersScan() {
                 <Spin
                   indicator={
                     <LoadingOutlined
-                      style={{ fontSize: 24, color: "#fff" }}
+                      style={{ fontSize: 70, color: "#fff" }}
                       spin
                     />
                   }
                 />
               ) : (
-                " Verify Tracking ID"
+                "Verify Tracking ID"
               )}
             </Button>
           </>
         ) : (
           <>
-            <p style={{ fontSize: "18px" }}>
+            <p>
               🔢 Scanning Part {currentPartIndex + 1} of{" "}
               {order.entries.flatMap((entry) => entry.partNumbers).length}
             </p>
             <Input
               value={scannedPart}
               onChange={(e) => setScannedPart(e.target.value)}
+              onKeyPress={(e) => handleKeyPress(e, "part")}
+              onPaste={(e) => handlePaste(e, "part")} // pass the event
               placeholder="📌 Scan Part Number"
-              style={{ fontSize: "16px", padding: "10px" }}
+              style={{ fontSize: "70px", padding: "10px" }}
+              autoFocus
             />
+
             <Button
               type="primary"
               style={{
                 marginTop: "10px",
                 width: "100%",
-                fontSize: "18px",
-                padding: "15px 20px",
+                fontSize: "40px",
+                padding: "40px 40px 90px",
                 paddingBottom: "40px",
                 fontWeight: "bold",
                 borderRadius: "8px",
                 backgroundColor: "#1890ff",
                 border: "none",
-                transition: "all 0.3s ease",
               }}
-              onMouseEnter={(e) => (e.target.style.backgroundColor = "#1890ff")}
-              onMouseLeave={(e) => (e.target.style.backgroundColor = "#1890ff")}
               onClick={verifyPartNumber}
               disabled={loading}
             >
@@ -204,13 +303,13 @@ function OrdersScan() {
                 <Spin
                   indicator={
                     <LoadingOutlined
-                      style={{ fontSize: 24, color: "#fff" }}
+                      style={{ fontSize: 70, color: "#fff" }}
                       spin
                     />
                   }
                 />
               ) : (
-                " Verify Part Number"
+                "Verify Part No"
               )}
             </Button>
           </>
